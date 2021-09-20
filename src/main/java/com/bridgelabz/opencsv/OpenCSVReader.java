ceaae6fbe7a25c55e6c1780d4383ebeb7b18819e
Swathi <swathi.h@perfios.com>
2021-09-20 17:30:06 +0530

    MODIFIED: Reading the file all at once

--- src/main/java/com/bridgelabz/opencsv/OpenCSVReader.java
+++ src/main/java/com/bridgelabz/opencsv/OpenCSVReader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
@@ -16,13 +17,13 @@ public class OpenCSVReader {
 		try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
 				CSVReader csvReader = new CSVReader(reader);) {
 
-			String[] nextRecord;
-			while ((nextRecord = csvReader.readNext()) != null) {
-				System.out.println("Name: " + nextRecord[0]);
-				System.out.println("Email: " + nextRecord[1]);
-				System.out.println("Phone: " + nextRecord[2]);
-				System.out.println("Country: " + nextRecord[3]);
-				System.out.println("===============================");
+			List<String[]> records = csvReader.readAll();
+			for (String[] record : records) {
+				System.out.println("Name: " + record[0]);
+				System.out.println("Email: " + record[1]);
+				System.out.println("Phone: " + record[2]);
+				System.out.println("Country: " + record[3]);
+				System.out.println("-------------------------------");
 			}
 
 		}
