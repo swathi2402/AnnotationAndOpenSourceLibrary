918ab39020655356ff9553a4aabd4f1b05142187
Swathi <swathi.h@perfios.com>
2021-09-20 18:34:25 +0530

    MODIFIED: Reading all CSV contents into memory

--- src/main/java/com/bridgelabz/opencsv/OpenCSVReadAndParseToBean.java
+++ src/main/java/com/bridgelabz/opencsv/OpenCSVReadAndParseToBean.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.List;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -14,21 +14,19 @@ public class OpenCSVReadAndParseToBean {
 	public static final String SAMPLE_CSV_FILE_PATH = "./users.csv";
 
 	public static void main(String[] args) throws IOException {
-		try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-		) {
+		try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));) {
 			CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder(reader).withType(CSVUser.class)
 					.withIgnoreLeadingWhiteSpace(true).build();
 
-			Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
-			
-			while (csvUserIterator.hasNext()) {
-				CSVUser csvUser = csvUserIterator.next();
+			List<CSVUser> csvUsers = csvToBean.parse();
+			for (CSVUser csvUser : csvUsers) {
 				System.out.println("Name: " + csvUser.getName());
 				System.out.println("Email: " + csvUser.getEmail());
 				System.out.println("Phone Number: " + csvUser.getPhoneNo());
 				System.out.println("Country: " + csvUser.getCountry());
-				System.out.println("==============================");
+				System.out.println("-------------------------------");
 			}
+
 		}
 
 	}
